9b3976e7a6b1bcbc9e67ef9d7817a49a4ac208d6
corahowell <corarusk@outlook.com>
2019-09-16 13:35:31 -0500

    Completed assignment 15 album info

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
-  $('album-title').text(album.title);
+  $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('.artist').text(album.artist);
+  $('#releaseInfo').text(album.releaseInfo);
 }
